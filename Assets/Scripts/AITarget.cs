dcb31b0a9ac150d5d8fac7f5c0708e5f863b03fb
danielnaval6 <danielnaval.20@campuscamara.es>
2022-04-27 14:55:52 +0200

    nueva clase aitarget

---
+++ Assets/Scripts/AITarget.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AITarget : MonoBehaviour
+{
+    [Header("Properties")]
+    //
+    [SerializeField] private Transform target;
+    private Rigidbody _rb;
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _agent.destination = target.position;
+    }
+}
