d1da6858d1cadefe16cd5a1c10b0a32039474632
danielnaval6 <danielnaval.20@campuscamara.es>
2022-05-27 08:54:19 +0200

    Implementada máquina expendedora. Cambiada la resolución de ciertos materiales. Arreglado bug en el que la pistola no comenzaría con cargadores aleatorios para testear.

--- Assets/Scripts/Weapons/MagazinesGun.cs
+++ Assets/Scripts/Weapons/MagazinesGun.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class MagazinesGun : BaseWeapon, IWeapon, IGun
 {
-    [Space(10)]
-    [Header("Recoil Settings")]
+    [Space( 10 )]
+    [Header( "Recoil Settings" )]
     //
     [SerializeField] private float recoilSpeed;
     [SerializeField] private float recoverSpeed;
     [SerializeField] private Vector3 recoil;
     private static CamShake _camShake;
 
-    [Space(10)]
-    [Header("Ammunition Settings")]
+    [Space( 10 )]
+    [Header( "Ammunition Settings" )]
     //
     [SerializeField] private bool hasInfiniteAmmo;
     [SerializeField] private int currentLoadedRounds;
-    [SerializeField, Min(1)] private int maxMagazineCapacity;
-    [SerializeField, Min(1)] private int maxAmountOfMagazines;
-    [Space(2)]
+    [SerializeField, Min( 1 )] private int maxMagazineCapacity;
+    [SerializeField, Min( 1 )] private int maxAmountOfMagazines;
+    [Space( 2 )]
     [SerializeField] private List<int> magazines = new List<int>();
     private const int ONE_IN_THE_CHAMBER = 1;
 
@@ -31,7 +31,7 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
             if (hasInfiniteAmmo) return;
             //else...
 
-            currentLoadedRounds = Mathf.Clamp(value, 0, maxMagazineCapacity + ONE_IN_THE_CHAMBER);
+            currentLoadedRounds = Mathf.Clamp( value, 0, maxMagazineCapacity + ONE_IN_THE_CHAMBER );
         }
     }
     public int MaxMagazineCapacity => maxMagazineCapacity;
@@ -49,7 +49,7 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
         set
         {
             _isAiming = value;
-            AnimatorController.SetBool("Aim", value);
+            AnimatorController.SetBool( "Aim", value );
         }
     }
 
@@ -73,7 +73,7 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(RaycastOrigin.position, RaycastOrigin.forward * MeleeRange);
+        Gizmos.DrawRay( RaycastOrigin.position, RaycastOrigin.forward * MeleeRange );
     }
 
     #endregion
@@ -84,41 +84,41 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
     public virtual void MeleeAttack()
     {
-        bool animatorIsNotIdle = !AnimatorController.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+        bool animatorIsNotIdle = !AnimatorController.GetCurrentAnimatorStateInfo( 0 ).IsName( "Idle" );
         if (animatorIsNotIdle) return;
         //else...
 
-        AnimatorController.SetTrigger("Melee");
+        AnimatorController.SetTrigger( "Melee" );
 
         _camShake.ShakeCamera();
 
-        Ray meleeRay = 
-            new Ray(origin: RaycastOrigin.position,
-                    direction: RaycastOrigin.forward);
+        Ray meleeRay =
+            new Ray( origin: RaycastOrigin.position,
+                    direction: RaycastOrigin.forward );
         bool objectIsNotInRange =
-            !Physics.Raycast(ray: meleeRay,
+            !Physics.Raycast( ray: meleeRay,
                              hitInfo: out RaycastHit hit,
                              maxDistance: MeleeRange,
                              layerMask: AttackLayers,
-                             queryTriggerInteraction: QueryTriggerInteraction.Ignore);
+                             queryTriggerInteraction: QueryTriggerInteraction.Ignore );
 
         if (objectIsNotInRange) return;
         //else...
 
-        PushRigidbodyFromRaycastHit(hit, MeleeForce);
+        PushRigidbodyFromRaycastHit( hit, MeleeForce );
 
-        bool hitDoesntHaveLivingThingsTag = !hit.transform.CompareTag("LivingThings");
+        bool hitDoesntHaveLivingThingsTag = !hit.transform.CompareTag( "LivingThings" );
         if (hitDoesntHaveLivingThingsTag) return;
         //else...
 
-        GetLivingThingFromTransform(hit.transform, out LivingThing target);
+        GetLivingThingFromTransform( hit.transform, out LivingThing target );
         target.Health -= MeleeDamage;
-        target.BleedFromRaycastHit(hit);
+        target.BleedFromRaycastHit( hit );
     }
 
     public virtual void Fire()
     {
-        bool cantFire = !AnimatorController.GetCurrentAnimatorStateInfo(0).IsTag("CanFire");
+        bool cantFire = !AnimatorController.GetCurrentAnimatorStateInfo( 0 ).IsTag( "CanFire" );
         bool thereAreBulletsInMag = CurrentLoadedRounds > 0;
 
         if (cantFire) return;
@@ -126,51 +126,51 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
         if (thereAreBulletsInMag)
         {
-            AnimatorController.SetTrigger("Shoot");
+            AnimatorController.SetTrigger( "Shoot" );
 
             for (int i = 0; i < PelletsPerShot; i++)
             {
-                Ray shot = 
-                    new Ray(origin: RaycastOrigin.position,
-                            direction: ApplySpreadToDirection(RaycastOrigin.forward));
-                bool objectIsNotInRange = 
-                    !Physics.Raycast(ray: shot,
+                Ray shot =
+                    new Ray( origin: RaycastOrigin.position,
+                            direction: ApplySpreadToDirection( RaycastOrigin.forward ) );
+                bool objectIsNotInRange =
+                    !Physics.Raycast( ray: shot,
                                      hitInfo: out RaycastHit hit,
                                      maxDistance: WeaponRange,
                                      layerMask: AttackLayers,
-                                     queryTriggerInteraction: QueryTriggerInteraction.Ignore);
+                                     queryTriggerInteraction: QueryTriggerInteraction.Ignore );
 
                 if (objectIsNotInRange) continue;
                 //else...
-                
-                PushRigidbodyFromRaycastHit(hit, WeaponForce / PelletsPerShot);
 
-                bool targetHasLivingThingTag = hit.transform.CompareTag("LivingThings");
+                PushRigidbodyFromRaycastHit( hit, WeaponForce / PelletsPerShot );
+
+                bool targetHasLivingThingTag = hit.transform.CompareTag( "LivingThings" );
                 if (targetHasLivingThingTag)
                 {
-                    GetLivingThingFromTransform(hit.transform, out LivingThing target);
+                    GetLivingThingFromTransform( hit.transform, out LivingThing target );
 
                     target.Health -= WeaponDamage / PelletsPerShot;
-                    target.BleedFromRaycastHit(hit);
+                    target.BleedFromRaycastHit( hit );
                 }
                 else
                 {
-                    SpawnRandomBulletHole(hit);
+                    SpawnRandomBulletHole( hit );
                 }
             }
 
             bool lastRound = CurrentLoadedRounds == 1;
             if (lastRound)
             {
-                AnimatorController.SetLayerWeight(1, 1);
+                AnimatorController.SetLayerWeight( 1, 1 );
             }
 
             CurrentLoadedRounds--;
-            _camShake.ShakeCamera(recoil, recoilSpeed, recoverSpeed);
+            _camShake.ShakeCamera( recoil, recoilSpeed, recoverSpeed );
         }
         else
         {
-            AnimatorController.SetTrigger("FailedShot");
+            AnimatorController.SetTrigger( "FailedShot" );
 
             //play an empty mag sound
         }
@@ -181,7 +181,7 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
     public virtual void Reload()
     {
-        bool animatorIsNotIdle = !AnimatorController.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+        bool animatorIsNotIdle = !AnimatorController.GetCurrentAnimatorStateInfo( 0 ).IsName( "Idle" );
         if (animatorIsNotIdle) return;
         //else...
 
@@ -197,16 +197,16 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
                 ///keep one round in the chamber
                 ///save the used mag in pocket
 
-                if (magHasMoreThanOneRound) Magazines.Add(CurrentLoadedRounds - ONE_IN_THE_CHAMBER);
+                if (magHasMoreThanOneRound) Magazines.Add( CurrentLoadedRounds - ONE_IN_THE_CHAMBER );
 
-                AnimatorController.SetTrigger("NoRackReload");
+                AnimatorController.SetTrigger( "NoRackReload" );
             }
             else
             {
-                AnimatorController.SetLayerWeight(1, 0);
-                AnimatorController.SetTrigger("RackReload");
+                AnimatorController.SetLayerWeight( 1, 0 );
+                AnimatorController.SetTrigger( "RackReload" );
             }
-            
+
             //sorts list from least to greatest
             Magazines.Sort();
 
@@ -215,14 +215,14 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
             if (theresStillRoundsInMag)
             {
-                CurrentLoadedRounds = Magazines[lastMemberOfList] + ONE_IN_THE_CHAMBER;
+                CurrentLoadedRounds = Magazines[ lastMemberOfList ] + ONE_IN_THE_CHAMBER;
             }
             else
             {
-                CurrentLoadedRounds = Magazines[lastMemberOfList];
+                CurrentLoadedRounds = Magazines[ lastMemberOfList ];
             }
 
-            Magazines.RemoveAt(lastMemberOfList);
+            Magazines.RemoveAt( lastMemberOfList );
         }
         else
         {
@@ -232,8 +232,8 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
     public virtual void CheckAmmo()
     {
-        bool animatorIsIdle = !AnimatorController.GetCurrentAnimatorStateInfo(0).IsName("Idle");
-        bool thereAreNoRounds = AnimatorController.GetInteger("Rounds") <= 0;
+        bool animatorIsIdle = !AnimatorController.GetCurrentAnimatorStateInfo( 0 ).IsName( "Idle" );
+        bool thereAreNoRounds = AnimatorController.GetInteger( "Rounds" ) <= 0;
         string mags = Magazines.Count switch
         {
             1 => "mag",
@@ -248,17 +248,17 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
         if (animatorIsIdle) return;
         //else...
 
-        AmmoChecker.PrintMessage(message);
+        AmmoChecker.PrintMessage( message );
 
         if (thereAreNoRounds) return;
         //else...
 
-        AnimatorController.SetTrigger("CheckAmmo");
+        AnimatorController.SetTrigger( "CheckAmmo" );
     }
 
     #endregion
 
-    public void AddMagazine(int bulletAmount)
+    public void AddMagazine( int bulletAmount )
     {
         bool magazineListIsFull = Magazines.Count >= MaxAmountOfMagazines;
 
@@ -266,10 +266,10 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
         if (magazineListIsFull)
         {
-            Magazines.RemoveAt(0);
+            Magazines.RemoveAt( 0 );
         }
 
-        Magazines.Add(bulletAmount);
+        Magazines.Add( bulletAmount );
     }
 
     #endregion
@@ -277,8 +277,8 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
 
     private void UpdateAnimatorParameters()
     {
-        AnimatorController.SetInteger("Rounds", CurrentLoadedRounds);
-        AnimatorController.SetInteger("Magazines", Magazines.Count);
+        AnimatorController.SetInteger( "Rounds", CurrentLoadedRounds );
+        AnimatorController.SetInteger( "Magazines", Magazines.Count );
     }
 
     private void FillGunWithRandomMags()
@@ -287,10 +287,10 @@ public class MagazinesGun : BaseWeapon, IWeapon, IGun
         if (magsShouldntBeSpawned) return;
         //else...
 
-        for (int i = 0; i < Magazines.Capacity; i++)
+        for (int i = 0; i < MaxAmountOfMagazines; i++)
         {
-            int randomAmountOfBullets = Random.Range(1, MaxMagazineCapacity + 1);
-            Magazines.Add(randomAmountOfBullets);
+            int randomAmountOfBullets = Random.Range( 0, MaxMagazineCapacity ) + 1;
+            Magazines.Add( randomAmountOfBullets );
         }
     }
 
