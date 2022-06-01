606fb1b2daa1791e4a19c59313475fab394595ea
danielnaval6 <danielnaval.20@campuscamara.es>
2022-06-01 14:02:57 +0200

    Cambio de texturas, implementado mapa

--- Assets/Scripts/LightOptions.cs
+++ Assets/Scripts/LightOptions.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class LightOptions : MonoBehaviour
 {
-    [Header("Properties")]
+    [Header( "Properties" )]
     //
-    [SerializeField] private AnimationCurve curve;
-    [SerializeField] private float time;
+    private float _defaultLightIntensity;
+
     private Light _light;
 
     #region MonoBehaviour
@@ -15,29 +15,42 @@ public class LightOptions : MonoBehaviour
     private void Awake()
     {
         _light = GetComponent<Light>();
+        _defaultLightIntensity = _light.intensity;
     }
 
     #endregion
 
     #region Public methods
 
-    public void SetLightColorR(float r)
+    public void TurnOn()
     {
-        r = Mathf.Clamp01(r);
-        _light.color = new Color(r, _light.color.g, _light.color.b);
+        _light.intensity = _defaultLightIntensity;
     }
 
-    public void SetLightColorG(float g)
+    public void TurnOff()
     {
-        g = Mathf.Clamp01(g);
-        _light.color = new Color(_light.color.r, g, _light.color.b);
+        _light.intensity = 0;
     }
 
-    public void SetLightColorB(float b)
+    public void SetLightColorR( float r )
     {
-        b = Mathf.Clamp01(b);
-        _light.color = new Color(_light.color.r, _light.color.g, b);
+        r = Mathf.Clamp01( r );
+        _light.color = new Color( r, _light.color.g, _light.color.b );
     }
+
+    public void SetLightColorG( float g )
+    {
+        g = Mathf.Clamp01( g );
+        _light.color = new Color( _light.color.r, g, _light.color.b );
+    }
+
+    public void SetLightColorB( float b )
+    {
+        b = Mathf.Clamp01( b );
+        _light.color = new Color( _light.color.r, _light.color.g, b );
+    }
+
+
 
     #endregion
 }
